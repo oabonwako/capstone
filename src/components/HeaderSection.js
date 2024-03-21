28cc6c1829ed0e14946038a34e5fcaa2d0a4d42f
Oabo <o.nwako36@gmail.com>
2024-03-21 19:08:36 +0100

    made corrections

---
+++ src/components/HeaderSection.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+function HeaderSection() {
+    return(
+        <div>
+            <header>
+                <div className="textbox">
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>
+                        We are a family owned Mediterranean restaurant,
+                        focused on traditional recipes served with a modern twist.
+                    </p>
+                    <button><Link to='/booking'>Reserve a Table</Link></button>
+                </div>
+                <div className="imagebox"></div>
+            </header>
+        </div>
+    );
+}
+
+export default HeaderSection;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,41 @@
 function Main() {
-    return;
+    return(
+        <main>
+                <article className="headsection">
+                    <h2>This Weeks Special!</h2>
+                    <button>Online Menu</button>
+                </article>
+                <article class="card-container">
+                        <div class="cardbody">
+                            <div className='card-pic1'></div>
+                            <div className='cardheader'>
+                                <h4>Greek Salad</h4>
+                                <h5>$12.00</h5>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque non risus vel ultricies. Vivamus velit augue, commodo eget hendrerit at, volutpat non neque. Donec sodales malesuada diam a varius. Aenean fringilla, sapien vel egestas varius, eros nibh scelerisque ipsum, ac ornare enim nulla molestie magna.</p>
+                            <a href='oabo.com'>Order a Delivery</a>
+                        </div>
+                        <div class="cardbody">
+                            <div className='card-pic2'></div>
+                            <div className='cardheader'>
+                                <h4>Bruchetta</h4>
+                                <h5>$12.00</h5>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque non risus vel ultricies. Vivamus velit augue, commodo eget hendrerit at, volutpat non neque. Donec sodales malesuada diam a varius. Aenean fringilla, sapien vel egestas varius, eros nibh scelerisque ipsum, ac ornare enim nulla molestie magna.</p>
+                            <a href='oabo.com'>Order a Delivery</a>
+                        </div>
+                        <div class="cardbody">
+                            <div className='card-pic3'></div>
+                            <div className='cardheader'>
+                                <h4>Lemon Dessert</h4>
+                                <h5>$12.00</h5>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque non risus vel ultricies. Vivamus velit augue, commodo eget hendrerit at, volutpat non neque. Donec sodales malesuada diam a varius. Aenean fringilla, sapien vel egestas varius, eros nibh scelerisque ipsum, ac ornare enim nulla molestie magna.</p>
+                            <a href='oabo.com'>Order a Delivery</a>
+                        </div>
+                    </article>
+            </main>
+    );
 };
 
 export default Main;
